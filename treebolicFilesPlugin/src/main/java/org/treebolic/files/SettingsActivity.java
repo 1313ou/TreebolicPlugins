37863c20d4ece2f91ed63f4ae50389c8d67d34a3
bbou <bbou@ac-toulouse.fr>
2019-08-29 15:34:52 +0200

    theming materialcomponents infer nullity remove settings lib

--- treebolicFilesPlugin/src/main/java/org/treebolic/files/SettingsActivity.java
+++ treebolicFilesPlugin/src/main/java/org/treebolic/files/SettingsActivity.java
@@ -1,90 +1,24 @@
 package org.treebolic.files;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import org.treebolic.AppCompatCommonPreferenceActivity;
 import org.treebolic.TreebolicIface;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.legacy.contrib.Header;
 
 /**
- * A AppCompatPreferenceActivity that presents a set of application settings.
+ * Settings activity
+ *
+ * @author Bernard Bou
  */
 public class SettingsActivity extends AppCompatCommonPreferenceActivity
 {
-	// E V E N T S
-
-	@Override
-	protected void onCreate(final Bundle savedInstanceState)
-	{
-		// super
-		super.onCreate(savedInstanceState);
-
-		// toolbar
-		final Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-
-		// set up the action bar
-		final ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null)
-		{
-			actionBar.setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_HOME_AS_UP);
-		}
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(@NonNull final MenuItem item)
-	{
-		if (item.getItemId() == android.R.id.home)
-		{
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	// S E T U P
-
-	@Override
-	public void onBuildHeaders(@NonNull final List<Header> target)
-	{
-		loadHeadersFromResource(R.xml.pref_headers, target);
-	}
-
-	// D E T E C T I O N
-
-	@Override
-	public boolean isValidFragment(final String fragmentName)
-	{
-		return GeneralPreferenceFragment.class.getName().equals(fragmentName);
-	}
-
-	@Override
-	public boolean onIsMultiPane()
-	{
-		return SettingsActivity.isLargeTablet(this);
-	}
-
-	/**
-	 * Helper method to determine if the device has an extra-large screen. For example, 10" tablets are extra-large.
-	 */
-	private static boolean isLargeTablet(@NonNull final Context context)
-	{
-		return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
-	}
-
 	// S U M M A R Y
 
 	private static final Preference.SummaryProvider<Preference> STRING_SUMMARY_PROVIDER = (preference) -> {
